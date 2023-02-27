166bf822432786ca69d196443b1479f92b7fecc0
Alexander Albert <alex.albert@vilkrig.com>
2023-02-27 22:50:54 +0100

    Add Unit Test

--- sole_trader_classifier.py
+++ sole_trader_classifier.py
@@ -5,13 +5,15 @@ import torch.optim as optim
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 # Load the customer data
-df = pd.read_csv('customer_data.csv')
+df = pd.read_csv("customer_data.csv")
 
 # Preprocess the data
 le = LabelEncoder()
-df['business_indicators'] = le.fit_transform(df['business_indicators'])
+df["business_indicators"] = le.fit_transform(df["business_indicators"])
 ohe = OneHotEncoder()
-contextual_info_ohe = ohe.fit_transform(df['contextual_info'].values.reshape(-1, 1)).toarray()
+contextual_info_ohe = ohe.fit_transform(
+    df["contextual_info"].values.reshape(-1, 1)
+).toarray()
 
 # Create the neural network model
 class SoleTraderClassifier(nn.Module):
@@ -21,7 +23,7 @@ class SoleTraderClassifier(nn.Module):
         self.fc2 = nn.Linear(16, 8)
         self.fc3 = nn.Linear(8 + num_contextual_info_categories, 1)
         self.sigmoid = nn.Sigmoid()
-        
+
     def forward(self, x, c):
         x = self.fc1(x)
         x = nn.functional.relu(x)
@@ -33,6 +35,7 @@ class SoleTraderClassifier(nn.Module):
         x = self.sigmoid(x)
         return x
 
+
 # Train the model
 num_features = 2
 num_contextual_info_categories = contextual_info_ohe.shape[1]
@@ -41,8 +44,10 @@ optimizer = optim.Adam(model.parameters(), lr=0.01)
 criterion = nn.BCELoss()
 
 for epoch in range(1000):
-    inputs = torch.tensor(df[['transaction_history', 'business_indicators']].values, dtype=torch.float32)
-    labels = torch.tensor(df['is_sole_trader'].values, dtype=torch.float32).unsqueeze(1)
+    inputs = torch.tensor(
+        df[["transaction_history", "business_indicators"]].values, dtype=torch.float32
+    )
+    labels = torch.tensor(df["is_sole_trader"].values, dtype=torch.float32).unsqueeze(1)
     contextual_info = contextual_info_ohe.astype(float)
     outputs = model(inputs, contextual_info)
     loss = criterion(outputs, labels)
@@ -52,11 +57,13 @@ for epoch in range(1000):
 
 # Evaluate the model
 with torch.no_grad():
-    inputs = torch.tensor(df[['transaction_history', 'business_indicators']].values, dtype=torch.float32)
-    labels = torch.tensor(df['is_sole_trader'].values, dtype=torch.float32).unsqueeze(1)
+    inputs = torch.tensor(
+        df[["transaction_history", "business_indicators"]].values, dtype=torch.float32
+    )
     contextual_info = contextual_info_ohe.astype(float)
     outputs = model(inputs, contextual_info)
     predicted_labels = (outputs >= 0.5).squeeze().numpy()
-    accuracy = (predicted_labels == df['is_sole_trader'].values).mean()
+    df["is_sole_trader"] = predicted_labels
 
-print('Accuracy: {:.2f}%'.format(accuracy * 100))
+# Save the result
+df.to_csv("classified_customer_data.csv", index=False)
---
+++ test_classification.py
@@ -0,0 +1,19 @@
+import unittest
+import pandas as pd
+from sole_trader_classifier import SoleTraderClassifier
+
+class TestSoleTraderClassifier(unittest.TestCase):
+    
+    def test_classification(self):
+        # Load the test data
+        test_data = pd.read_csv('test_data.csv')
+        expected_results = test_data['is_sole_trader']
+        
+        # Create the classifier
+        classifier = SoleTraderClassifier()
+        
+        # Make predictions and compare with expected results
+        predicted_results = classifier.predict(test_data)
+        self.assertEqual(len(predicted_results), len(expected_results))
+        for i in range(len(predicted_results)):
+            self.assertEqual(predicted_results[i], expected_results[i])
